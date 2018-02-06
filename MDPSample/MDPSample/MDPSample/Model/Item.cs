d2715e456be7e558cfa2b43cf5a61d41d7fb4f27
Achraf Kadiri Daidi <achraf.kadiri-daidi@sogeti.com>
2018-02-06 12:34:12 +0100

    Implemented master detail with 3 pages.

---
+++ MDPSample/MDPSample/MDPSample/Model/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDPSample.Model
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string IconSource { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
